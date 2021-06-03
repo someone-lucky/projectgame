515d5a034076d614d61921498f763ee2f5926936
Максим Егоров <meegorov@miem.hse.ru>
2021-06-03 23:46:58 +0300

    cmake change+SFML add

--- project/main.cpp
+++ project/main.cpp
@@ -3,7 +3,25 @@
 using namespace sf;
 
 int main() {
-    RenderWindow window(VideoMode(500,500), "Game");
+    RenderWindow scrn(VideoMode(500,500), "Game");
+    Image heroim;
+    heroim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\hero.png");
 
+    Texture hero;
+    hero.loadFromImage(heroim);
+
+    Sprite h;
+    h.setTexture(hero);
+    h.setPosition(50,100);
+    while (scrn.isOpen()){
+        Event ev;
+        while (scrn.pollEvent(ev)){
+            if (ev.type == Event::Closed);
+            scrn.close();
+        }
+        scrn.clear();
+        scrn.draw(h);
+        scrn.display();
+    }
     return 0;
 }
