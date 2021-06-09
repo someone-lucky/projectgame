48d0e5c45c9202a69cd1b6f4f875c0de36840ed1
Максим Егоров <meegorov@miem.hse.ru>
2021-06-09 20:58:25 +0300

    add

--- project/main.cpp
+++ project/main.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 int main() {
     RenderWindow scrn(VideoMode(500,500), "Game");
     Image heroim;
-    heroim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\hero.png");
+    heroim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\main.png");
 
     Texture hero;
     hero.loadFromImage(heroim);
@@ -14,10 +14,19 @@ int main() {
     h.setTexture(hero);
     h.setPosition(50,100);
     while (scrn.isOpen()){
-        Event ev;
-        while (scrn.pollEvent(ev)){
-            if (ev.type == Event::Closed);
-            scrn.close();
+        //Event ev;
+        //while (scrn.pollEvent(ev)){
+        //    if (ev.type == Event::Closed);
+        //    scrn.close();
+        //}
+        if (Keyboard::isKeyPressed(Keyboard::Right)){
+            h.move(0.5,0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Left)){
+            h.move(-0.5,0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Up)){
+            h.move(0,-0.5);
         }
         scrn.clear();
         scrn.draw(h);
