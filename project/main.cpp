c51b5180dd835a0a3ea3f4c9074672a175b0cbf8
Максим Егоров <meegorov@miem.hse.ru>
2021-06-09 20:52:44 +0300

    add

--- project/main.cpp
+++ project/main.cpp
@@ -3,21 +3,31 @@
 using namespace sf;
 
 int main() {
-    RenderWindow scrn(VideoMode(500,500), "Game");
+    RenderWindow scrn(VideoMode(700,700), "Game");
     Image heroim;
-    heroim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\hero.png");
+    heroim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\main.png");
 
     Texture hero;
     hero.loadFromImage(heroim);
 
     Sprite h;
     h.setTexture(hero);
-    h.setPosition(50,100);
+    h.setPosition(100,100);
+
     while (scrn.isOpen()){
         Event ev;
-        while (scrn.pollEvent(ev)){
-            if (ev.type == Event::Closed);
-            scrn.close();
+        //while (scrn.pollEvent(ev)){
+         //   if (ev.type == Event::Closed);
+         //   scrn.close();
+       // }
+        if (Keyboard::isKeyPressed(Keyboard::Right)){
+            h.move(0.5,0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Left)){
+            h.move(-0.5,0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Up)){
+            h.move(0,-0.5);
         }
         scrn.clear();
         scrn.draw(h);
