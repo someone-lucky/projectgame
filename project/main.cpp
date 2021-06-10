4aa93fcda8f47d48220027396f3232aed61d7523
Максим Егоров <meegorov@miem.hse.ru>
2021-06-10 21:59:11 +0300

    add

--- project/main.cpp
+++ project/main.cpp
@@ -32,21 +32,44 @@ public:
         now=0;
         spr.setTexture(image);
         spr.setTextureRect(IntRect(10,15,50,65));
-        coord = FloatRect(0,zemlya+65,50,65);
+        coord = FloatRect(100,100,50,65);
+    }
+    void StuckX(){
+        for (int i=coord.top/32; i<(coord.top+coord.height)/32; i++)
+            for (int j=coord.left/32; j<(coord.left+coord.width)/32; j++){
+                if (FrameMap[i][j] == 'A'){
+                    if (vx>0)
+                        coord.left=j*32-coord.width;
+                    if (vx<0)
+                        coord.left=j*32+32;
+                }
+            }
+    }
+    void StuckY(){
+        for (int i=coord.top/32; i<(coord.top+coord.height)/32; i++)
+            for (int j=coord.left/32; j<(coord.left+coord.width)/32; j++){
+                if (FrameMap[i][j] == 'A'){
+                    if (vy>0) {
+                        coord.top = i * 32 - coord.height;
+                        vy = 0;
+                        on = true;
+                    }
+                    if (vy<0) {
+                        coord.left = i * 32 + 32;
+                        vy = 0;
+                    }
+                }
+            }
     }
 
     void checkup(float timer){
         coord.left += vx*timer;
+        StuckX();
         if (!on)
             vy=vy+0.0000000005*timer;
         coord.top += vy*timer;
-        if (coord.top > zemlya - coord.height) {
-            coord.top = zemlya - coord.height;
-            vy = 0;
-            on = true;
-        }
-        else
-            on = false;
+        //on=false;
+        StuckY();
         now += 0.00001*timer;
         if (now > 4)
             now -= 4;
@@ -67,13 +90,17 @@ int main() {
     Image heroim;
     heroim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\main.png");
     heroim.createMaskFromColor(Color(229, 159, 159));
+    Image block;
+    block.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\Tile_31.png");
 
     Texture texture;
     texture.loadFromImage(heroim);
+    Texture t1;
+    t1.loadFromImage(block);
+    Sprite bs1;
+    bs1.setTexture(t1);
 
     hero h(texture);
-
-    RectangleShape block;
 
     Clock tik;
 
@@ -103,12 +130,11 @@ int main() {
         for (int i=0; i<hm; i++)
             for (int j=0; j<lm; j++){
                 if (FrameMap[i][j] == 'A')
-                    block.setFillColor(Color::Black);
-                block.setSize({32.0,32.0});
+                    bs1.setTextureRect(IntRect(0,0,32,32));
                 if (FrameMap[i][j] == ' ')
                     continue;
-                block.setPosition(j*32,i*32);
-                scrn.draw(block);
+                bs1.setPosition(j*32,i*32);
+                scrn.draw(bs1);
             }
         scrn.draw(h.spr);
         scrn.display();
