28a668869ae49241b405f12e5af5a17f571216be
Максим Егоров <meegorov@miem.hse.ru>
2021-06-13 20:20:53 +0300

    add

--- project/doctest/tests_main.cpp
+++ project/doctest/tests_main.cpp
@@ -58,6 +58,16 @@ TEST_CASE("Checkup run tests: gravity") {
     hero.checkup(timer);
     REQUIRE(hero.coord.top == 590);  // 590 - 50 = 540, но учитывая гравитацию +50 = 590 - верно! - в итоге не сдвинулся
     REQUIRE(hero.coord.top != 480);
+    hero.coord = FloatRect(64,640,50,64);
+    hero.vy = -5;
+    for (int i=0; i<30; i++){
+        hero.g = 0.5;
+        timer = 1;
+        hero.on = false;
+        hero.checkup(timer);
+    }
+    REQUIRE(hero.coord.top == 640);
+    REQUIRE(hero.on);
 }
 TEST_CASE("Checkup collision tests") {
     Image heroim;
@@ -79,4 +89,43 @@ TEST_CASE("Checkup collision tests") {
         hero.checkup(timer);
     }
     REQUIRE(hero.coord.top != 140);
+    hero.coord = FloatRect(64,650,50,64);
+    hero.checkup(timer);
+    REQUIRE(hero.coord.top == 640);
+}
+TEST_CASE("Checkup enemy tests") {
+    Image evilim;
+    evilim.loadFromFile("C:\\Users\\66236\\CLionProjects\\projectgame\\project\\evil.png");
+    evilim.createMaskFromColor(Color(229, 159, 159));
+    Texture texture1;
+    texture1.loadFromImage(evilim);
+    enemy evil(texture1);
+    evil.living = true;
+    evil.vx = 10;
+    timer = 10;
+    evil.coord = FloatRect(64,640,45, 38);
+    evil.checkup(timer);
+    REQUIRE(evil.coord.left == 164);
+    REQUIRE(evil.coord.left != 163);
+    evil.vx = -7;
+    evil.checkup(timer);
+    REQUIRE(int(evil.coord.left) == 94);
+    REQUIRE(evil.coord.left != 163);
+    evil.checkup(timer);
+    REQUIRE(int(evil.coord.left) == 94);
+    evil.vx = 3;
+    evil.checkup(timer);
+    REQUIRE(int(evil.coord.left) == 124);
+    REQUIRE(evil.coord.left != 70);
+    evil.vx = 3;
+    evil.checkup(timer);
+    REQUIRE(evil.vx == float(0.0002));
+    evil.coord = FloatRect(500,640,45, 38);
+    for (int i=0; i<50; i++){
+        evil.vx = -10;
+        timer = 1;
+        evil.checkup(timer);
+    }
+    REQUIRE(evil.coord.top != 140);
+    REQUIRE(evil.vx == float(0.0002));
 }
